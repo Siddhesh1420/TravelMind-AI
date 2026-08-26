df4cce73896d4c46ea68f4d42251546273cec406
Siddhesh1420 <siddheshdiptil@gmail.com>
2026-08-26 23:10:14 +0530

    Updated tools

--- backend/tools/hotels.py
+++ backend/tools/hotels.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv()
 
-def search_hotels(destination,check_in,check_out,rating,free_cancellation,currency="INR",sort_by=3,adults=1,children=0,min_price=None,max_price=None):
+def search_hotels(destination,check_in,check_out,rating,currency="INR",sort_by=3,adults=1,children=0,max_price=None):
     """ 
     Search for hotels based on above details  """
     
@@ -14,10 +14,8 @@ def search_hotels(destination,check_in,check_out,rating,free_cancellation,curren
         "check_in_date":check_in,
         "check_out_date":check_out,
         "currency":currency,
-        "min_price":min_price,
         "max_price":max_price,
         "rating":rating,
-        "free_cancellation":free_cancellation,
         "adults":adults,
         "children":children,
         "sort_by":sort_by,
@@ -39,15 +37,13 @@ if __name__=="__main__":
     check_in=input("Enter check-in date (YYYY-MM-DD): ")
     check_out=input("Enter check-out date (YYYY-MM-DD): ")
     currency=input("Enter currency (default INR): ")
-    min_price=int(input("Enter minimum price: "))
     max_price=int(input("Enter maximum price: "))
     rating=int(input("Enter minimum rating (7: 3.5+ , 8: 4.0+ , 9: 4.5+ ): "))
-    free_cancellation=input("Require free cancellation? (yes/no): ").lower() == "yes"
     adults=int(input("Enter number of adults: "))
     children=int(input("Enter number of children: "))
     sort_by=int(input("Sort by (3=Price, 8=Rating, 13=Most Reviewed): "))
 
-    hotels=search_hotels(destination,check_in,check_out,rating,free_cancellation,currency,sort_by,adults,children,min_price,max_price)
+    hotels=search_hotels(destination,check_in,check_out,rating,currency,sort_by,adults,children,max_price)
     print(hotels)
         
         
