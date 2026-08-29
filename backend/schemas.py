b6f0608e6ba7ab9184e7bd67429d1018b64fee18
Siddhesh1420 <siddheshdiptil@gmail.com>
2026-08-29 09:56:17 +0530

    Made HostelInfo schema

--- backend/schemas.py
+++ backend/schemas.py
@@ -9,7 +9,7 @@ class TripInput(BaseModel):
     budget: int=Field(...,description="Total budget for the trip in INR")
     group_size: int=Field(...,description="Number of people in a group")
     travel_mode: str=Field(...,description="Mode of travel (e.g., flight, train, bus, car)")
-    preferences: List[str]=Field(...,description="List of preferences like . eg.['vegetarian','adventure','budget-friendly']")
+    preferences: Optional[List[str]]=Field([],description="List of preferences like . eg.['vegetarian','adventure','budget-friendly']")
     phone_number: Optional[str]=Field(None,description="Phone number of the user")
     user_id: Optional[str]=Field(None,description="Email or unique identifier of the user")
     
@@ -36,10 +36,14 @@ class TrainInfo(BaseModel):
     departure_time: str=Field(...,description="Departure time in HH:MM format")
     arrival_time: str=Field(...,description="Arrival time in HH::MM format")
     duration: str=Field(...,description="Duration in HH::MM format")
-    classes: List[str]=Field(...,description="Llist of classes")
+    classes: List[str]=Field(...,description="List of classes")
     days: str=Field(...,description="Days on which train run . eg: Daily or 'MTWTFSS' ")
     
 class HotelInfo(BaseModel):
     name: str=Field(...,description="Name of hotel")
     rating: float=Field(...,description="Give rating out of 5")
-    
+    price_per_night: str=Field(...,description="Price_per_night")
+    total_price: str=Field(...,description="Total price")
+    amenities: List[str]=Field(...,description="List of amenities")
+    check_in_time: str=Field(...,description="Check in time")
+    check_out_time: str=Field(...,description="Check out time")
